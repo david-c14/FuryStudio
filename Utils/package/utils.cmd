2bada1af42b9452c191f4005d0ee426f940bf875
David O'Rourke <david-c14@users.noreply.github.com>
2023-06-05 16:29:13 +0100

    Revert "Use scripts for packaging artifacts"
    This reverts commit de57a80554afbfa7ce6783be5d76fc270cce03c1.

--- Utils/package/utils.cmd
+++
@@ -1,7 +0,0 @@
-mkdir utils
-copy ..\build\Debug\DatFile.exe utils
-copy ..\build\Debug\ImmFile.exe utils
-copy ..\..\LICENSE utils
-copy ..\..\doc\utilities\datfile.md utils
-copy ..\..\doc\utilities\immfile.md utils
-Compress-Archive -Path ".\utils\*" -DestinationPath "Utils-win-x64-${{ github.event.release.name }}.zip"
